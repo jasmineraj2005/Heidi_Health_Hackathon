0c984ecc3262cced19393e83d5af6fc5ecdda339
Long Dang <hoanglongdang2001@gmail.com>
2025-07-12 14:14:41 +1000

    Add support for markdown in main render zone

--- heidi_hack_frontend/src/components/MiddleRenderZone.jsx
+++ heidi_hack_frontend/src/components/MiddleRenderZone.jsx
@@ -1,10 +1,13 @@
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 function MiddleRenderZone({ messages }) {
   return (
     <div className="middle-zone">
       <div className="chat-history">
         {messages.map((msg, i) => (
           <div key={i} className={`message ${msg.role}`}>
-            {msg.text}
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{msg.text}</ReactMarkdown>
           </div>
         ))}
       </div>
